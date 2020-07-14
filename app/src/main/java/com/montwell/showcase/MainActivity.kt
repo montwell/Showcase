4f5e7f1ffe72308548eca5f71452fdc2ef67e77f
Jonathan Montwell <montwell@gmail.com>
2020-07-14 08:24:54 -0400

    first commit

---
+++ app/src/main/java/com/montwell/showcase/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.montwell.showcase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.montwell.showcase.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
